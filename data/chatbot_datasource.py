2c72b52b01a96c7face2505b03a898a05fc0762d
erik-gzn <erik.gritzan@gmail.com>
2025-01-17 20:25:04 +0100

    Datenbank erstellt

---
+++ data/chatbot_datasource.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+# Erstellung der Datenbank ###############################################
+
+# def setup_database():
+#     conn = sqlite3.connect('chatbot.db')
+#     cursor = conn.cursor()
+
+#     # Create table
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS chatbot_data (
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             service_keywords TEXT NOT NULL,
+#             question TEXT NOT NULL,
+#             answer TEXT NOT NULL
+#         )
+#     ''')
+
+#     # Commit and close
+#     conn.commit()
+#     conn.close()
+
+# setup_database() 
+###########################################################################
+
+
+
