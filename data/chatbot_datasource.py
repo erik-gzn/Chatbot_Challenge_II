5454f31bc79126bc75d48b0393541d1ae7a120c2
erik-gzn <erik.gritzan@gmail.com>
2025-01-18 14:21:09 +0100

    Grundfunktionen (Datenbank + Zugriff + Dateistruktur)

--- data/chatbot_datasource.py
+++ data/chatbot_datasource.py
@@ -1,27 +1,55 @@
+# Setup the database for the chatbot service
+
 import sqlite3
 
-# Erstellung der Datenbank ###############################################
+def setup_database():
+    conn = sqlite3.connect('chatbot_service_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS chatbot_data (
+            id INTEGER PRIMARY KEY,
+            tag_one TEXT,
+            tag_two TEXT,
+            tag_three TEXT,
+            answer TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-# def setup_database():
-#     conn = sqlite3.connect('chatbot.db')
-#     cursor = conn.cursor()
+def insert_data(tag_one, tag_two, tag_three, answer):
+    conn = sqlite3.connect('chatbot_service_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO chatbot_data (tag_one, tag_two, tag_three, answer)
+        VALUES (?, ?, ?, ?)
+    ''', (tag_one, tag_two, tag_three, answer))
+    conn.commit()
+    conn.close()
 
-#     # Create table
-#     cursor.execute('''
-#         CREATE TABLE IF NOT EXISTS chatbot_data (
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             service_keywords TEXT NOT NULL,
-#             question TEXT NOT NULL,
-#             answer TEXT NOT NULL
-#         )
-#     ''')
+def access_data(tag=None):
+    conn = sqlite3.connect('chatbot_service_data.db')
+    cursor = conn.cursor()
+    if tag:
+        cursor.execute('''
+            SELECT answer FROM chatbot_data
+            WHERE tag_one = ? OR tag_two = ? OR tag_three = ?
+        ''', (tag, tag, tag))
+    else:
+        cursor.execute('SELECT answer FROM chatbot_data')
+    rows = cursor.fetchall()
+    conn.close()
+    return [row[0] for row in rows]
 
-#     # Commit and close
-#     conn.commit()
-#     conn.close()
+def delete_data(id):
+    conn = sqlite3.connect('chatbot_service_data.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM chatbot_data WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
 
-# setup_database() 
-###########################################################################
 
+#setup_database()
 
-
+# Daten in die Datenbank einfügen 
+#insert_data('support', 'broken', '', 'We are sorry to hear that your device is broken and concerned to help you! Would you like us to book support or order parts for you?.')
--- logic/chatbot_logic.py
+++ logic/chatbot_logic.py
@@ -1,2 +1,6 @@
+from data.chatbot_datasource import access_data
+
 def get_response(input_to_answer):
-    return "So you have " + input_to_answer + "?"
+    
+    return str(access_data(input_to_answer))
+    #return "So you have " + input_to_answer + "?"
